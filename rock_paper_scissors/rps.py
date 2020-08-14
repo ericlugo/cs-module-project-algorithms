af0e1a45e46cf879b71b05d9fbe496c8822f0834
Eric SarragaLugo <ericlugo@users.noreply.github.com>
2020-08-14 03:39:40 -0400

    rps.py complete

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,29 +1,28 @@
 import sys
 
-
 def rock_paper_scissors(n):
-    choices = {
-        1: "rock",
-        2: "paper",
-        3: "scissors",
-    }
-    result_grid = []
-    for _ in choices:
-        result_grid.append(rps_helper(n, result_grid, choices))
-    print(result_grid)
+    result = []
+    return rock_paper_scissors_helper(n, result)
 
-
-def rps_helper(plays, end_res, choices, cur_res=[]):
-    if plays == 0:
-        print(cur_res)
-        return end_res.append(cur_res)
+def rock_paper_scissors_helper(n, result, choices=["rock", "paper", "scissors"], current_list=[]):
+    if n == 0:
+        result.append(current_list[:])  # GOAL
     else:
-        print(plays)
-        for key in choices.keys():
-            # print(choices[key])
-            cur_res.append(choices[key])
-            return rps_helper(plays-1, end_res, choices, cur_res)
+        for index in range(3):
+            current_list.append(choices[index])  # CHOICE
+            rock_paper_scissors_helper(n-1, result, choices, current_list)  # CONSTRAINT
+            current_list.pop(len(current_list)-1)  # BACKTRACKING
+    return result
 
+# def rock_paper_scissors(n, result=[], choices=["rock", "paper", "scissors"], current_list=[]):
+#     if n == 0:
+#         result.append(current_list[:])  # GOAL
+#     else:
+#         for index in range(3):
+#             current_list.append(choices[index])  # CHOICE
+#             rock_paper_scissors(n-1, result, choices, current_list)  # CONSTRAINT
+#             current_list.pop(len(current_list)-1)  # BACKTRACKING
+#     return result
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
@@ -31,18 +30,3 @@ if __name__ == "__main__":
         print(rock_paper_scissors(num_plays))
     else:
         print('Usage: rps.py [num_plays]')
-
-"""
-rock_paper_scissors(n):
-    - create a dictionary corresponding to the three playable choices
-    _ create the primary array that will house all response arrays
-    - call the helper with the number of items per array, the dictionary
-        that it will use to create the play arrays, and a cache to memoize
-        the possible outcomes since it is likely that many options will
-        repeat during generation.
-
-rps_helper(n, response, cache = {}):
-    - if length==0 return current_response
-    - else:
-
-"""
